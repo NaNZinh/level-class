5fab654476739652c3eb2ef95e8369393330a831
NaNZinh <luan.r.geronimo@gmail.com>
2026-03-22 11:30:01 -0300

    correções do código

--- index.js
+++ index.js
@@ -5,32 +5,25 @@ let nivelDoHeroi = "";
 if (xpDoHeroi <= 1000) {
   nivelDoHeroi = "Ferro";
 }
-elif(xpDoHeroi > 1000 && xpDoHeroi <= 2000);
-{
+else if(xpDoHeroi > 1000 && xpDoHeroi <= 2000){
   nivelDoHeroi = "Bronze";
 }
-elif(xpDoHeroi > 2000 && xpDoHeroi <= 5000);
-{
+else if(xpDoHeroi > 2000 && xpDoHeroi <= 5000){
   nivelDoHeroi = "Prata";
 }
-elif(xpDoHeroi > 5000 && xpDoHeroi <= 7000);
-{
+else if(xpDoHeroi > 5000 && xpDoHeroi <= 7000){
   nivelDoHeroi = "Ouro";
 }
-elif(xpDoHeroi > 7000 && xpDoHeroi <= 8000);
-{
+else if(xpDoHeroi > 7000 && xpDoHeroi <= 8000){
   nivelDoHeroi = "Platina";
 }
-elif(xpDoHeroi > 8000 && xpDoHeroi <= 9000);
-{
+else if(xpDoHeroi > 8000 && xpDoHeroi <= 9000){
   nivelDoHeroi = "Ascendente";
 }
-elif(xpDoHeroi > 9000 && xpDoHeroi <= 10000);
-{
+else if(xpDoHeroi > 9000 && xpDoHeroi <= 10000){
   nivelDoHeroi = "Imortal";
 }
-elif(xpDoHeroi > 10000);
-{
+else if(xpDoHeroi > 10000){
   nivelDoHeroi = "Radiante";
 }
 console.log(
